2e93613e9e88b67d49567014da93147913c09d7a
Tabisa2024 <tabisatinga@gmail.com>
2024-11-14 15:49:55 +0200

    End of section 4

--- Application/Activities/Details.cs
+++ Application/Activities/Details.cs
@@ -1,21 +1,19 @@
-using System;
-using System.Threading;
 using System.Threading.Tasks;
-using System.Diagnostics;
 using MediatR;
 using Persistence;
+using Domain;
 
-
-namespace Application
+namespace Application.Activities
 {
     public class Details
     {
-public class Query : IRequest<Activity>
-{
-    public Guid Id { get; set; }
-}
+      public class Query : IRequest<Activity>
+      {
+        public Guid Id { get; set; }
+      }
         public class Handler : IRequestHandler<Query, Activity>
         {
+            private readonly DataContext _context;
             public Handler(DataContext context)
             {
                 _context = context;
@@ -23,7 +21,7 @@ public class Query : IRequest<Activity>
 
             public async Task<Activity> Handle(Query request, CancellationToken cancellationToken)
             {
-               return await _context.Activities.FindAsync(request.Id);
+                return await _context.Activities.FindAsync(request.Id);
             }
         }
     }
